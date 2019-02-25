abea70bf0de91e96ddaabc22f7c1a9731be84aae
hassanaster <hassanaster2@gmail.com>
2019-02-26 00:20:10 +0100

    Ulyimo commit

--- src/main/java/com/example/demo/LabInyeccionDependencias.java
+++ src/main/java/com/example/demo/LabInyeccionDependencias.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class LabInyeccionDependencias {
 
-	public static void main (String [ ] args) {
-
-		 
-
- System.out.println ("Empezamos la ejecución del programa");
-
- 
-
-    } 
-	
+	public static void main(String[] args) {
+		SpringApplication.run(LabInyeccionDependencias.class, args);
+	}
 }
